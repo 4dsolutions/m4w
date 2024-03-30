5f40d424ef0994b56537a99ae59128034e4d9373
Kirby Urner <kirby.urner@gmail.com>
2024-03-30 12:35:39 -0700

    push sympy compatibility

--- tetravolume.py
+++ tetravolume.py
@@ -3,6 +3,10 @@ Euler volume, modified by Gerald de Jong
 http://www.grunch.net/synergetics/quadvols.html
 Kirby Urner (c) MIT License
 
+March 30, 2024: tighten the unittests to rely more on sympy
+March 30, 2024: save edges and angles when initializing the Tetrahedron
+March 30, 2024: make_tet now returns Tetrahedon (not volumes tuple)
+
 The tetravolume.py methods make_tet and make_tri 
 assume that volume and area use R-edge cubes and 
 triangles for XYZ units respectively, and D-edge 
@@ -22,7 +26,7 @@ sqrt(2) to the 3rd power.  One third of that volume
 is our unit tetrahedron of edges D (cube face diagonals).
 
 See:
-http://mathforum.org/kb/thread.jspa?threadID=2836546
+https://coda.io/d/Math4Wisdom_d0SvdI3KSto/ivm-xyz_suqdu#_luR7B
 for explanation of quadrays, used for some unit tests
 
 A goal for this version of tetravolume.py + qrays.py
@@ -64,7 +68,7 @@ https://www.omnicalculator.com/math/triangle-angle
 """
 
 import sympy as sp
-from sympy import Rational, Integer, acos, deg
+from sympy import Rational, Integer, acos, deg, N
 from sympy import sqrt as rt2
 from qrays import Qvector, Vector
 import sys
@@ -78,6 +82,11 @@ root3 = rt2(3)
 root5 = rt2(5)
 root6 = rt2(6)
 PHI = (1 + root5)/2
+
+Smod = (PHI **-5)/2  
+Emod = (root2/8) * (PHI ** -3)
+
+sfactor = Smod/Emod
 
 class Tetrahedron:
     """
@@ -225,9 +234,8 @@ def make_tet(v0,v1,v2):
     """
     three edges from any corner, remaining three edges computed
     """
-    tet = Tetrahedron(v0.length(), v1.length(), v2.length(), 
+    return Tetrahedron(v0.length(), v1.length(), v2.length(), 
                       (v0-v1).length(), (v1-v2).length(), (v2-v0).length())
-    return tet.ivm_volume(), tet.xyz_volume()
 
 class Triangle:
     
@@ -270,122 +278,138 @@ class Test_Tetrahedron(unittest.TestCase):
         e4 = rt2(5 - 2*root5)
         e5 = 1/PHI
         tet = Tetrahedron(e0, e1, e2, e3, e4, e5)
-        self.assertTrue(1/23 > tet.ivm_volume()/8 > 1/24, "Wrong E-mod")
+        self.assertTrue(sp.Eq(tet.ivm_volume()/8, 
+                               (root2/8) * (PHI ** -3)))
+        
+    def test_s_module(self):
+        e0 = 1/PHI
+        e1 = sfactor/2
+        e2 = root3 * e1/2
+        e3 = (3 - root5)/2
+        e4 = e1/2
+        e5 = e4
+        tet = Tetrahedron(e0, e1, e2, e3, e4, e5)
+        self.assertTrue(sp.Eq(tet.ivm_volume(), 
+                               (PHI ** -5)/2))
         
     def test_unit_volume2(self):
         tet = Tetrahedron(R, R, R, R, R, R)
-        self.assertAlmostEqual(tet.xyz_volume(), 0.117851130)
+        self.assertTrue(sp.Eq(tet.xyz_volume(), root2/12))
 
     def test_unit_volume3(self):
         tet = Tetrahedron(R, R, R, R, R, R)
-        self.assertAlmostEqual(tet.ivm_volume(), 0.125)
+        self.assertTrue(sp.Eq(tet.ivm_volume(), Rational(1,8)))
         
     def test_phi_edge_tetra(self):
         tet = Tetrahedron(D, D, D, D, D, PHI)
-        self.assertAlmostEqual(float(tet.ivm_volume()), 0.70710678)
+        self.assertTrue(sp.Eq(tet.ivm_volume(), root2/2))
 
     def test_right_tetra(self):
-        e = pow((root3/2)**2 + (root3/2)**2, 0.5)  # right tetrahedron
+        e = rt2((root3/2)**2 + (root3/2)**2)  # right tetrahedron
         tet = Tetrahedron(D, D, D, D, D, e)
-        self.assertAlmostEqual(tet.xyz_volume(), 1)
+        self.assertEqual(tet.xyz_volume(), D)
 
     def test_quadrant(self):
         qA = Qvector((1,0,0,0))
         qB = Qvector((0,1,0,0))
         qC = Qvector((0,0,1,0))
         tet = make_tet(qA, qB, qC) 
-        self.assertAlmostEqual(tet[0], 0.25) 
+        self.assertEqual(tet.ivm_volume(), Rational(1,4)) 
 
     def test_octant(self):
         x = Vector((R, 0, 0))
         y = Vector((0, R, 0))
         z = Vector((0, 0, R))
         tet = make_tet(x,y,z)
-        self.assertAlmostEqual(tet[1], 1/6, 5) # good to 5 places
+        self.assertEqual(tet.xyz_volume(), Rational(1,6))
 
     def test_quarter_octahedron(self):
         a = Vector((D,0,0))
         b = Vector((0,D,0))
         c = Vector((R,R,root2/2))
         tet = make_tet(a, b, c)
-        self.assertAlmostEqual(tet[0], 1, 5) # good to 5 places  
+        self.assertEqual(tet.ivm_volume(), D) 
 
     def test_xyz_cube(self):
-        a = Vector((R, 0.0, 0.0))
-        b = Vector((0.0, R, 0.0))
-        c = Vector((0.0, 0.0, R))
+        a = Vector((R,0,0))
+        b = Vector((0,R,0))
+        c = Vector((0,0,R))
         R_octa = make_tet(a,b,c) 
-        self.assertAlmostEqual(6 * R_octa[1], 1, 4) # good to 4 places  
+        self.assertEqual(6 * R_octa.xyz_volume(), D) # good to 4 places  
 
     def test_s3(self):
         D_tet = Tetrahedron(D, D, D, D, D, D)
-        a = Vector((R, 0.0, 0.0))
-        b = Vector((0.0, R, 0.0))
-        c = Vector((0.0, 0.0, R))
-        R_cube = 6 * make_tet(a,b,c)[1]
-        self.assertAlmostEqual(D_tet.xyz_volume() * S3, R_cube, 4)
+        a = Vector((R,0,0))
+        b = Vector((0,R,0))
+        c = Vector((0,0,R))
+        R_cube = 6 * make_tet(a,b,c).xyz_volume()
+        self.assertEqual(D_tet.xyz_volume() * S3, R_cube)
 
     def test_martian(self):
-        p = Qvector((2,1,0,1))
-        q = Qvector((2,1,1,0))
-        r = Qvector((2,0,1,1))
+        two = Integer(2)
+        one = Integer(1)
+        p = Qvector((two,one,0,one))
+        q = Qvector((two,one,one,0))
+        r = Qvector((two,0,one,one))
         result = make_tet(5*q, 2*p, 2*r)
-        self.assertAlmostEqual(result[0], 20, 7)
+        self.assertEqual(result.ivm_volume(), Integer(20))
         
     def test_area_martian1(self):
-        p = Qvector((2,1,0,1))
-        q = Qvector((2,1,1,0))
+        two = Integer(2)
+        one = Integer(1)
+        p = Qvector((two,one,0,one))
+        q = Qvector((two,one,one,0))
         result = p.area(q)
-        self.assertAlmostEqual(result, 1)        
+        self.assertEqual(result, D)        
  
     def test_area_martian2(self):
-        p = 3 * Qvector((2,1,0,1))
-        q = 4 * Qvector((2,1,1,0))
+        two = Integer(2)
+        one = Integer(1)
+        p = 3 * Qvector((two,one,0,one))
+        q = 4 * Qvector((two,one,one,0))
         result = p.area(q)
-        self.assertAlmostEqual(result, 12)
+        self.assertEqual(result, 12)
 
     def test_area_martian3(self):
         qx = Vector((D,0,0)).quadray()
         qy = Vector((R,rt2(3)/2,0)).quadray()
         result = qx.area(qy)
-        self.assertAlmostEqual(result, 1, 7)
+        self.assertEqual(result, D)
         
     def test_area_earthling1(self):
         vx = Vector((1,0,0))
         vy = Vector((0,1,0))
         result = vx.area(vy)
-        self.assertAlmostEqual(result, 1)        
+        self.assertEqual(result, 1)        
 
     def test_area_earthling2(self):
         vx = Vector((2,0,0))
         vy = Vector((1,rt2(3),0))
         result = vx.area(vy)
-        self.assertAlmostEqual(result, 2*rt2(3))    
+        self.assertEqual(result, 2*rt2(3))    
         
     def test_phi_tet(self):
         "edges from common vertex: phi, 1/phi, 1"
         p = Vector((1, 0, 0))
-        q = Vector((1, 0, 0)).rotz(60) * PHI
-        r = Vector((0.5, root3/6, root6/3)) * 1/PHI
+        q = Vector((1, 0, 0)).rotz(60) * PHI # rotation is still a bit fuzzy
+        r = Vector((Rational(1,2), root3/6, root6/3)) * 1/PHI
         result = make_tet(p, q, r)
-        self.assertAlmostEqual(result[0], 1, 7)
+        self.assertAlmostEqual(N(result.ivm_volume()), D)
         
     def test_phi_tet_2(self):
-        p = Qvector((2,1,0,1))
-        q = Qvector((2,1,1,0))
-        r = Qvector((2,0,1,1))
+        two = Integer(2)
+        one = Integer(1)
+        p = Qvector((two,one,0,one))
+        q = Qvector((two,one,one,0))
+        r = Qvector((two,0,one,one))
         result = make_tet(PHI*q, (1/PHI)*p, r)
-        # print("Phi_tet_2", result[0])      
-        # self.assertAlmostEqual(result[0], 1, 7)
-        self.assertAlmostEqual(result[0].evalf(), 1, 7)
+        self.assertTrue(sp.Eq(result.ivm_volume(), D))
         
     def test_phi_tet_3(self):
-        T = Tetrahedron(PHI, 1/PHI, 1.0, 
+        T = Tetrahedron(PHI, 1/PHI, 1, 
                         root2, root2/PHI, root2)
         result = T.ivm_volume()
-        # print("Phi_tet_3", result) 
-        # self.assertAlmostEqual(result, 1, 7)
-        self.assertAlmostEqual(result.evalf(), 1, 7)
+        self.assertTrue(sp.Eq(result, D))
 
     def test_koski(self):
         a = 1 
@@ -396,7 +420,7 @@ class Test_Tetrahedron(unittest.TestCase):
         f = (root2) * PHI ** -1       
         T = Tetrahedron(a,b,c,d,e,f)
         result = T.ivm_volume()
-        self.assertAlmostEqual(result, PHI ** -3, 7)       
+        self.assertTrue(sp.Eq(result, PHI ** -3))      
 
 class Test_Triangle(unittest.TestCase):
     
